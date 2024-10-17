9b4592efbf441626682f7b1f7deb78a933037f6a
Matt Sephton <matt.sephton@gmail.com>
2024-10-17 21:37:53 +0100

    1.0.0

--- Unwrap Paragraph.novaextension/Scripts/main.js
+++ Unwrap Paragraph.novaextension/Scripts/main.js
@@ -1,14 +1,12 @@
 exports.activate = function() {
-    nova.commands.register("com.gingerbeardman.reformat", flowLines);
+    nova.commands.register("com.gingerbeardman.unwraptext", flowLines);
 }
 
 function flowLines(editor) {
-    const selectedRanges = editor.selectedRanges;
-    
     editor.edit(function(e) {
-        for (let range of selectedRanges.reverse()) {
+        for (let range of editor.selectedRanges.reverse()) {
             let text = editor.getTextInRange(range);
-            let newText = text.replace(/\n/g, ' ').trim();
+            let newText = text.replace(/\s+/g, ' ').trim();
             e.replace(range, newText);
         }
     });
